c13d50779d96978ebf8ef9c258d92fcb79ba6bf4
Juan David Prada Malagon <pradamalagonjd@gmail.com>
2018-01-10 17:04:39 -0500

    Add files via upload

--- TransformadadeFourier1D.py
+++ TransformadadeFourier1D.py
@@ -1,11 +1,21 @@
 import numpy as np 
 
-def T_Fourier(z): # Entra por parametro un array Nx1 (256x1)
-	# Indices del Kernel
+## Lectura de Datos 
+
+Datos=np.loadtxt('funcion.dat')
+
+x = Datos[:,0] 
+y = Datos[:,1] 
+
+## Constantes 
+
+N = np.shape(Datos)[0]
+
+## Transformada de Fourier
+
+def T_Fourier(z): 
 	n = np.arange(N)
 	k = np.reshape(n, (N, 1))
-	# Matriz del Kernel de la transformacion NxN (256x256)
 	M_nk = np.exp(-(2j*np.pi*n*k)/N)
-	# Datos en el espacio de frecuencias Nx1 (256x1)
 	T_z = np.dot(M_nk, z)
 	return T_z
